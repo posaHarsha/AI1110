67ad18b6b138ba89b532181d17b1e3ab3dad6e00
posaHarsha <posaharshao11@gmail.com>
2023-05-10 17:46:46 +0530

    done

---
+++ Assign3/codes/stem.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt 
+x= [0,2,4,6]
+y= [0.3125,0.78125,0.96875,1]
+
+plt.stem(x,y)
+
+plt.show()
---
+++ Assign3/codes/stem1.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt 
+x= [0,1,2,3,4,5,6]
+y= [0.015625,0.109375,0.34375,0.65625,0.890625,0.984375,1]
+
+plt.stem(x,y)
+
+plt.show()
